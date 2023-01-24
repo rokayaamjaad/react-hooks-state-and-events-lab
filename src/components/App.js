426cf1f2f3804f62193d9b1568bcf13d5036e8f4
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-24 21:23:10 +0300

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+const [theme, setTheme] = useState(false);
+const appClass = theme ? "App dark" : "App light"
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  function handleLiClick() {
+  setTheme((theme) => !theme);
+
+}
+console.log(theme);
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button  onClick={handleLiClick}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
